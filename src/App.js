103bc9244a8fb7cb430e6965599f7beb8ff1e251
ShalithAlahakoon <shalitha97068@gmail.com>
2022-02-06 21:37:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+function Header(props){
+  return(
+    <header>
+      <h1>{props.name}'s kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props){
+  return(
+    <div>
+      <p>We provide you most {props.adjective} foods around.</p>
+    </div>
+  );
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <div>Copyright @ {props.year}</div>
+    </footer>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Shalihta"/>
+      <Main adjective="amaizing"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
